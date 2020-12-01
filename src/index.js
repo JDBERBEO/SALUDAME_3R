cfca668537790347542d029fec9c3401d3712b72
Juan <jd.berbeo@gmail.com>
2020-12-01 11:15:40 -0500

    version1

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express ();
+const path = require('path');
+const hbs = require ('express-handlebars');
+
+//setting engine 
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.engine('hbs', hbs({
+    defaultLayout: 'main',
+    layoutsDir:path.join(app.get('views'),'layouts'),
+    partialsDir:path.join(app.get('views'),'partials'),
+    extname:'.hbs'
+}))
+
+app.set('view engine', '.hbs')
+// middlewares
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.get('/',(req,res)=> {
+    res.render('home', {
+        nombre: 'Juan'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
+
+app.get('/user',(req,res)=>{
+    res.render('user')
+})
+
+app.post('/user', (req,res)=> {
+    res.render('greetings', {
+        name: req.body.name
+    })
+})
+app.listen(3000, ()=>console.log('Listening in port 3000'))
